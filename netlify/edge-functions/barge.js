83811e4ee36248f78702c8e89db091c81bdc9687
Rob AI <amphibionics87@gmail.com>
2022-12-10 13:43:31 +0800

    added redirect

--- netlify/edge-functions/barge.js
+++ netlify/edge-functions/barge.js
@@ -1,12 +1,11 @@
 export default async (request) => {
-  const url = new URL(request.url);
-  const date = url.searchParams.get('date') || '';
+  // const url = new URL(request.url);
+  // const date = url.searchParams.get('date') || '';
 
-  const redirectUrl = `https://ttt.c0m.workers.dev/bargeForm?date=${date}&t=${Date.now()}`;
+  // const redirectUrl = `https://ttt.c0m.workers.dev/bargeForm?date=${date}&t=${Date.now()}`;
   // return Response.redirect(redirectUrl, 302);
 
-  return new Response(redirectUrl, {
+  return new Response(request.url, {
     headers: { "content-type": "text/html" },
   });
-
 };
