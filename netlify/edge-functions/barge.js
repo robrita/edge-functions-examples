0b6f1b5fabf113e68f0c1dee7228ca6d2cc2ea94
Rob AI <amphibionics87@gmail.com>
2022-12-11 18:53:57 +0800

    added source and origin

--- netlify/edge-functions/barge.js
+++ netlify/edge-functions/barge.js
@@ -1,7 +1,9 @@
 export default async (request) => {
   const url = new URL(request.url);
   const date = url.searchParams.get('date') || '';
+  const source = url.searchParams.get('source') || '';
+  const origin = url.searchParams.get('origin') || '';
 
-  const redirectUrl = `https://ttt.c0m.workers.dev/bargeForm?date=${date}&t=${Date.now()}`;
+  const redirectUrl = `https://ttt.c0m.workers.dev/bargeForm?date=${date}&source=${source}&origin=${origin}&t=${Date.now()}`;
   return Response.redirect(redirectUrl, 302);
 };
